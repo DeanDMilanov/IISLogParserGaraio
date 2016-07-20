32a587432344d64bc4134f3f83d06d44c045e366
DeanDMilanov <dido.mg@gmail.com>
2016-07-21 01:58:49 +0300

    The previous tests were written using Xunit and did not show in test explorer. The body of the tests is the same, only the test framework is different

---
+++ Tests/IPExtractorTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class MyTestClass
+    {
+        Random rand = new Random();
+        string ipAddress;
+        private string GeneratePseudoRandomStrings(int minLength)
+        {
+            string seed = "the quick brown fox jumped over the lazy dog";
+            StringBuilder sb = new StringBuilder();
+            do
+            {
+                sb.Append(seed.Substring(rand.Next(seed.Length)));
+            } while (sb.Length < minLength);
+            sb.Append(" {0}");
+            return sb.ToString();
+        }
+
+        [TestMethod]
+        public void CanParseValidIPV4Address()
+        {
+            ipAddress = "10.10.2.18";
+            string feed = string.Format(GeneratePseudoRandomStrings(150), ipAddress);
+            IPExtractor extractor = new IPExtractor();
+            var ip = extractor.ExtractIPString(feed);
+            Assert.AreEqual(ipAddress, ip.Value);
+        }
+
+
+        [TestMethod]
+        public void ReturnsEmptyOptionIfNoIPV4AddressIsFound()
+        {
+            string feed = GeneratePseudoRandomStrings(150);
+            IPExtractor extractor = new IPExtractor();
+            var ip = extractor.ExtractIPString(feed);
+            Assert.IsTrue(ip.IsEmpty);
+        }
+
+        [TestMethod]
+        public void CanParseValidIPV6Address()
+        {
+            ipAddress = "2001:cdba:0000:0000:0000:0000:3257:9652";
+            string feed = string.Format(GeneratePseudoRandomStrings(250), ipAddress);
+            IPExtractor extractor = new IPExtractor();
+            var ip = extractor.ExtractIPString(feed);
+            Assert.AreEqual(ipAddress, ip.Value);
+        }
+
+
+        [TestMethod]
+        public void ReturnsEmptyOptionIfNoIPV6AddressIsFound()
+        {
+            string feed = GeneratePseudoRandomStrings(200);
+            IPExtractor extractor = new IPExtractor();
+            var ip = extractor.ExtractIPString(feed);
+            Assert.IsTrue(ip.IsEmpty);
+        }
+    }
+    
+}
---
+++ Tests/StatisticsGeneratorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Services.Interfaces;
+using Services;
+using Services.Implementation;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class StatisticsGeneratorTests
+    {
+        private Mock<IFQDNExtractor> fqdnMock = new Mock<IFQDNExtractor>();
+        private string log1 = "2016-02-15 14:26:03 10.10.2.18 GET /Admin/Setting/Media - 443 test@garaio.com 83.150.38.202 Mozilla/5.0+(Windows+NT+6.3;+WOW64)+AppleWebKit/537.36+(KHTML,+like+Gecko)+Chrome/48.0.2564.109+Safari/537.36 https://10.10.10.10/Admin/Setting/GeneralCommon 200 0 0 31921";
+        private string log2 = "2016 - 02 - 15 14:26:03 ::1 GET / bundles / scripts / mldywzht4qbdkpfc1dmyt3fi - woufuop5p4ofykbxhm1 v = tYcwlp7sC3Lprqv3RqjoxyBT7vHf11MDXPv5yNDPwXk1 443 test@garaio.com ::1 Mozilla / 5.0 + (compatible;+MSIE + 10.0;+Windows + NT + 6.2;+WOW64;+Trident / 6.0) https://localhost/Admin/Widget/ConfigureWidget?systemName=Widgets.GoogleAdwords 200 0 0 64";
+
+        public StatisticsGeneratorTests()
+        {
+            fqdnMock.Setup(c => c.ExtractFQDN("::1")).Returns(new Option<string>("loopback"));
+            fqdnMock.Setup(c => c.ExtractFQDN("10.10.2.18")).Returns(new Option<string>("private"));
+        }
+
+        [TestMethod]
+        public void GeneratesStatisticsWhenSuppliedWithDataContainingIPv4()
+        {
+            StatisticsGenerator generator = new StatisticsGenerator(new IPExtractor(), fqdnMock.Object);
+            var result = generator.GenerateStatistics(new[] { log1 });
+            Assert.IsTrue(result != null && result.Any());
+        }
+        [TestMethod]
+        public void GeneratesStatisticsWhenSuppliedWithDataContainingIPv6()
+        {
+            StatisticsGenerator generator = new StatisticsGenerator(new IPExtractor(), fqdnMock.Object);
+            var result = generator.GenerateStatistics(new[] { log2 });
+            Assert.IsTrue(result != null && result.Any());
+        }
+    }
+}
